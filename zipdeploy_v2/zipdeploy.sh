b894c031fb458c0a7e582dfd781f8b4b477fc300
camnick <33676695+camnick@users.noreply.github.com>
2022-01-12 07:05:32 -0800

    [SC-94] formatting, add token functionality for local dev

--- zipdeploy_v2/zipdeploy.sh
+++ zipdeploy_v2/zipdeploy.sh
@@ -2,8 +2,15 @@
 if [[ -z $1 ]] || [[ -z $2 ]]; then echo "error: target Traffic Manager Profile OR package path/name not set"; exit 1; fi
 TM_ID=$1
 PACKAGE_LOCATION=$2
-token=$(az account get-access-token | jq -r .accessToken)
-#token=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com' -H Metadata:true -s | jq -r .access_token)
+echo "Pulling token"
+if [ -z "$3" ]
+then
+  echo "Pulling token for GitHub Runner"
+  token=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com' -H Metadata:true -s | jq -r .access_token)
+else
+  echo "Pulling token for Local Machine"
+  token=$(az account get-access-token --resource "https://management.azure.com" | jq -r '.accessToken')
+fi
 ENDPOINT_INFO=$(curl -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X GET https://management.azure.com${TM_ID}?api-version=2018-04-01 | jq -r '{ host:  .properties.monitorConfig.customHeaders[] | select(.name=="host").value, endpoints: [ .properties.endpoints[].properties | select(.endpointStatus=="Enabled") ] }')
 SUBSCRIPTION_ID=$(echo $ENDPOINT_INFO | jq -r '.endpoints[0].targetResourceId ' | cut -d '/' -f3)
 HOST=$(jq -r .host <<<$ENDPOINT_INFO)
@@ -13,7 +20,6 @@ IP_ID=$(jq -r '.endpoints[0].targetResourceId' <<<$ENDPOINT_INFO)
 ALL_GATEWAYS=$(curl -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Network/applicationGateways?api-version=2021-03-01 )
 TARGET_ENDPOINTS=$(jq -r ".value[] | select(.properties.frontendIPConfigurations[].properties.publicIPAddress.id==\"$IP_ID\") | (.properties.httpListeners[] | if (.properties.hostNames | index(\"$HOST\") != null) and .properties.protocol == \"Https\" then .id else empty end) as \$listener_id | ( .properties.requestRoutingRules[].properties | select(.httpListener.id==\$listener_id).backendAddressPool.id ) as \$backend_id | .properties.backendAddressPools[] | select(.id==\$backend_id) | .properties.backendAddresses[].fqdn " <<<$ALL_GATEWAYS)
 APP_HOSTNAMES=()
-#for app in $(curl -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Network/applicationGateways?api-version=2021-03-01 | jq -r ".value[] | select(.properties.frontendIPConfigurations[].properties.publicIPAddress.id==\"$IP_ID\") | (.properties.httpListeners[] | if (.properties.hostNames | index(\"$HOST\") != null) and .properties.protocol == \"Https\" then .id else empty end) as \$listener_id | ( .properties.requestRoutingRules[].properties | select(.httpListener.id==\$listener_id).backendAddressPool.id ) as \$backend_id | .properties.backendAddressPools[] | select(.id==\$backend_id) | .properties.backendAddresses[].fqdn "); do
 for app in ${TARGET_ENDPOINTS}; do
   APPS+=($app)
 done
@@ -22,7 +28,8 @@ for app in ${APPS[*]}; do
   id=$(curl -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X GET "https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/providers/Microsoft.Web/sites?api-version=2021-02-01" | jq -r  ".value[] | if (.properties.enabledHostNames | index(\"$app\") != null ) then .id else empty end") 
   APP_IDS+=($id)
 done
-CREDENTIALS=(); for app_id in ${APP_IDS[*]}; do  app_creds=$(curl -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X POST https://management.azure.com$app_id/config/publishingcredentials/list?api-version=2021-02-01 -d '' | jq -r .properties.scmUri); CREDENTIALS+=($app_creds); done
+CREDENTIALS=()
+for app_id in ${APP_IDS[*]}; do  app_creds=$(curl -H "Authorization: Bearer $token" -H "Content-Type: application/json" -X POST https://management.azure.com$app_id/config/publishingcredentials/list?api-version=2021-02-01 -d '' | jq -r .properties.scmUri); CREDENTIALS+=($app_creds); done
 for cred in ${CREDENTIALS[*]}; do
   curl -X POST --fail --data-binary @"${PACKAGE_LOCATION}" "$cred"/api/zipdeploy
 done
